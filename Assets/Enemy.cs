ade2b164132ff6d9fb54e85aa6c136d8ce5cb162
杨竞颉 <j.yang19@ejm.org>
2018-03-22 22:58:09 +0100

    Implement enemy movement AI

---
+++ Assets/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Speed = 10f;
+
+    private Transform _target;
+    private int _waypointIndex;
+
+    private void Start()
+    {
+        _target = Waypoints.Points[0];
+    }
+
+    private void Update()
+    {
+        var direction = _target.position - transform.position;
+        transform.Translate(direction.normalized * Speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, _target.position) <= .2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (_waypointIndex >= Waypoints.Points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _waypointIndex++;
+        _target = Waypoints.Points[_waypointIndex];
+    }
+}
---
+++ Assets/Waypoints.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] Points;
+
+    private void Awake()
+    {
+        Points = new Transform[transform.childCount];
+        for (var i = 0; i < Points.Length; i++)
+        {
+            Points[i] = transform.GetChild(i);
+        }
+    }
+}
