8ae8af3e8e1de8cd737c7ebbed80a94350b8cfc6
杨竞颉 <j.yang19@ejm.org>
2018-03-24 14:35:30 +0100

    Add bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform _target;
+
+    public float Speed = 70f;
+    public GameObject ImpactEffect;
+
+    public void Seek(Transform target)
+    {
+        _target = target;
+    }
+
+    private void Update()
+    {
+        if (_target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        var direction = _target.position - transform.position;
+        float distanceThisFrame = Speed * Time.deltaTime;
+        if (direction.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+    }
+
+    private void HitTarget()
+    {
+        var effectInstantce = Instantiate(ImpactEffect, transform.position, transform.rotation);
+        Destroy(effectInstantce, 1f);
+        Destroy(gameObject);
+
+        // TODO add HP
+        Destroy(_target.gameObject);
+    }
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,12 +4,24 @@
 public class Turret : MonoBehaviour
 {
     private Transform _target;
+
+    [Header("Attrubutes")]
+    // Turret attributes.
     public float Range = 15f;
 
+    // Number of bullets fired per second.
+    public float FireRate = 1f;
+    private float _fireCountDown = 0.25f;
+
+    [Header("Unity Setup Fields")]
+    // Fields that have to do with Unity.
     public string EnemyTag = "Enemy";
 
     public Transform PartToRotate;
     public float TurnSpeed = 10f;
+
+    public GameObject BulletPrefab;
+    public Transform FirePoint;
 
     private void Start()
     {
@@ -24,7 +36,7 @@ public class Turret : MonoBehaviour
         foreach (var enemy in enemies)
         {
             float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
-            if (!(distanceToEnemy < shortestDistance)) continue;
+            if (distanceToEnemy >= shortestDistance) continue;
             shortestDistance = distanceToEnemy;
             nearestEnemy = enemy;
         }
@@ -46,10 +58,30 @@ public class Turret : MonoBehaviour
             return;
         }
 
+        // Handle target lock-on.
         var direction = _target.position - transform.position;
         var lookRotation = Quaternion.LookRotation(direction);
         var rotation = Quaternion.Lerp(PartToRotate.rotation, lookRotation, Time.deltaTime * TurnSpeed).eulerAngles;
         PartToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        // Handle firing.
+        if (_fireCountDown <= 0)
+        {
+            Shoot();
+            _fireCountDown = 1 / FireRate;
+        }
+
+        _fireCountDown -= Time.deltaTime;
+    }
+
+    private void Shoot()
+    {
+        var bulletGameObj = Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
+        var bullet = bulletGameObj.GetComponent<Bullet>();
+        if (bullet != null)
+        {
+            bullet.Seek(_target);
+        }
     }
 
     private void OnDrawGizmosSelected()
