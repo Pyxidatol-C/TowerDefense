612020c499f1585e07a39a1b500df6ac7b2ada9b
杨竞颉 <j.yang19@ejm.org>
2018-03-24 11:56:04 +0100

    Add turrets and implement lock-on

---
+++ Assets/Turret.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+
+public class Turret : MonoBehaviour
+{
+    private Transform _target;
+    public float Range = 15f;
+
+    public string EnemyTag = "Enemy";
+
+    public Transform PartToRotate;
+    public float TurnSpeed = 10f;
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    private void UpdateTarget()
+    {
+        var enemies = GameObject.FindGameObjectsWithTag(EnemyTag);
+        var shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (var enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (!(distanceToEnemy < shortestDistance)) continue;
+            shortestDistance = distanceToEnemy;
+            nearestEnemy = enemy;
+        }
+
+        if (nearestEnemy != null && shortestDistance <= Range)
+        {
+            _target = nearestEnemy.transform;
+        }
+        else
+        {
+            _target = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (_target == null)
+        {
+            return;
+        }
+
+        var direction = _target.position - transform.position;
+        var lookRotation = Quaternion.LookRotation(direction);
+        var rotation = Quaternion.Lerp(PartToRotate.rotation, lookRotation, Time.deltaTime * TurnSpeed).eulerAngles;
+        PartToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
