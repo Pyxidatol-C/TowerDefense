7ffff46443a5eb90979480fadff1daedb45f690c
杨竞颉 <j.yang19@ejm.org>
2018-03-24 15:30:13 +0100

    Implement building turrets

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    // Instantialise singleton.
+    public static BuildManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene.");
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public GameObject StandardTurretPrefab;
+    private GameObject _turretToBuild;
+
+    private void Start()
+    {
+        _turretToBuild = StandardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return _turretToBuild;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color HoverColor;
+    public Vector3 PositionOffset;
+
+    private GameObject _turret;
+
+    private Renderer _renderer;
+    private Color _startColor;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _startColor = _renderer.material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        _renderer.material.color = HoverColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (_turret != null)
+        {
+            // TODO display on screen
+            Debug.Log("NOPE.");
+            return;
+        }
+
+        // Build a turret.
+        var turretToBuild = BuildManager.Instance.GetTurretToBuild();
+        _turret = Instantiate(turretToBuild, transform.position + PositionOffset, transform.rotation);
+    }
+
+    private void OnMouseExit()
+    {
+        _renderer.material.color = _startColor;
+    }
+}
